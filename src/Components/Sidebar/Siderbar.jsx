9c9addca6afe2c697ff2da927de35d28ebc4c85e
lochinbek9 <lochinbeksetor@gmail.com>
2025-10-01 20:55:42 +0500

    sidebar edited

--- src/Components/Sidebar/Siderbar.jsx
+++ src/Components/Sidebar/Siderbar.jsx
@@ -15,9 +15,9 @@ import tom from "../../assets/tom.png"
 import megan from "../../assets/megan.png"
 import cameron from "../../assets/cameron.png"
 
-function Siderbar() {
+function Siderbar({sidebar}) {
   return (
-    <div className="sidebar">
+    <div className={`sidebar ${sidebar?'':'small-sidebar'}`}>
       <div className="shortcut-links">
         <div className="side-link">
           <img src={home} alt="" /> <p>Home</p>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,10 @@
 import "./Home.css"
 import Sidebar from "../../Components/Sidebar/Siderbar"
 
-function Home() {
+function Home({sidebar}) {
   return (
     <>
-      <Sidebar />
+      <Sidebar sidebar={sidebar}/>
     </>
   )
 }
