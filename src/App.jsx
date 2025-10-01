6d297be5ae1dcd4f9941dd0270633bade51e823b
lochinbek9 <lochinbeksetor@gmail.com>
2025-10-01 20:28:28 +0500

    Sidebar section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 
+import { Route, Routes } from "react-router-dom"
 import Navbar from "./Components/Navbar/Navbar"
+import Home from "./Pages/Home/Home"
+import Video from "./Pages/Video/Video"
 function App() {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/video/:categoryId/:videoId" element={<Video/>} />
+
+      </Routes>
     </div>
   )
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,11 @@
 import "./Home.css"
+import Sidebar from "../../Components/Sidebar/Siderbar"
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+      <Sidebar />
+    </>
   )
 }
 
